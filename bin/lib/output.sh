e038db022f1a939ad081d18b7ef327589264e5f3
Wojciech Kania <wkania@davinci-studio.eu>
2020-09-27 20:40:32 +0200

    Shell: Info before the command that runs longer

--- bin/lib/output.sh
+++ bin/lib/output.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
 output () {
-    style_start=""
-    style_end=""
+    styleStart=""
+    styleEnd=""
+    newLine="\n"
+    if [ $# -ge 3 ]; then
+      newLine=""
+    fi
     if [ "${2:-}" != "" ]; then
     case $2 in
         "success")
-            style_start="\033[0;32m"
-            style_end="\033[0m"
+            styleStart="\033[0;32m"
+            styleEnd="\033[0m"
             ;;
         "error")
-            style_start="\033[31;31m"
-            style_end="\033[0m"
+            styleStart="\033[31;31m"
+            styleEnd="\033[0m"
             ;;
         "info"|"warning")
-            style_start="\033[33m"
-            style_end="\033[39m"
+            styleStart="\033[33m"
+            styleEnd="\033[39m"
             ;;
         "heading")
-            style_start="\033[1;33m"
-            style_end="\033[22;39m"
+            styleStart="\033[1;33m"
+            styleEnd="\033[22;39m"
             ;;
         "highlight")
-            style_start="\033[0;36m"
-            style_end="\033[22;39m"
+            styleStart="\033[0;36m"
+            styleEnd="\033[22;39m"
             ;;
     esac
     fi
 
-    printf "${style_start}${1}${style_end}\n"
+    printf "${styleStart}${1}${styleEnd}${newLine}"
 }
